601ec60a0c2cb2145bacfc12ee4d0e1793fb0ebf
Alessandro <alessandro.mpgh@gmail.com>
2021-07-26 23:12:25 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,23 @@
+var express = require("express");
+var translatte = require("translatte"); // Old school
+var cors = require('cors')
+var app = express()
+
+app.use(cors());
+
+traslate = '';
+
+app.get("/translate", (req, res, next) => {
+    var text = req.query.text;
+    var from = req.query.from;
+
+    translatte(text, {to: from}).then(res => {
+        this.traslate = res.text;
+    }).catch(err => {
+        console.error(err);
+    });
+    res.json( this.traslate);
+    });
+app.listen(3000, () => {
+ console.log("Server running on port 3000");
+});
